458a96372ca6db9656eaad40e81a2921953b087b
Anwesh43 <anweshthecool0@gmail.com>
2020-11-20 09:29:33 +0530

    adding RBLNode class

--- RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
+++ RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
@@ -125,4 +125,44 @@ class RisingBallLinerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RBLNode(var i : Int, val state : State = State()) {
+        private var next : RBLNode? = null
+        private var prev : RBLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RBLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRBLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RBLNode {
+            var curr : RBLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
