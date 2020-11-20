109f2664289c119bd651306e4cc6500c9c2df263
Anwesh43 <anweshthecool0@gmail.com>
2020-11-20 09:31:29 +0530

    adding RisingBallLiner class

--- RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
+++ RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
@@ -164,5 +164,28 @@ class RisingBallLinerView(ctx : Context) : View(ctx) {
             cb()
             return this
         }
+
+        data class RisingBallLiner(var i : Int) {
+
+            private var curr : RBLNode = RBLNode(0)
+            private var dir : Int = 1
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                curr.draw(canvas, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                curr.update {
+                    curr = curr.getNext(dir) {
+                        dir *= -1
+                    }
+                    cb(it)
+                }
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                curr.startUpdating(cb)
+            }
+        }
     }
 }
