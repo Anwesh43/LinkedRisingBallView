d6d7972fcb2abb6f80f8d50c0de30a322f46c2bd
Anwesh43 <anweshthecool0@gmail.com>
2020-11-20 09:22:44 +0530

    adding RisingBallLinerView class

--- RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
+++ RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawRBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawRisingBallLiner(scale, w, h, paint)
 }
+
+class RisingBallLinerView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
