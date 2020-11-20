9324ee8478be94d51b301cd9faea08f6dea854e3
Anwesh43 <anweshthecool0@gmail.com>
2020-11-20 09:44:47 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedrisingballlinerview/MainActivity.kt
+++ app/src/main/java/com/example/linkedrisingballlinerview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedrisingballlinerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.risingballlinerview.RisingBallLinerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RisingBallLinerView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
