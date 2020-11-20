4c724d0454faa4da9153ff9f1247fc23ca4617c6
Anwesh43 <anweshthecool0@gmail.com>
2020-11-20 09:21:36 +0530

    adding Canvas extension methods

--- RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
+++ RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
@@ -20,11 +20,45 @@ val colors : Array<Int> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.02f / parts
 val strokeFactor : Float = 90f
-val sizeFactor : Float = 2.9f
+val rFactor : Float = 5.4f
+val lineSizeFactor : Float = 9.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val lines : Int = 4
+val deg : Float = 360f
+val rot : Float = 60f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawRisingBallLiner(scale : Float, w : Float, h : Float, paint : Paint) {
+    val r : Float = Math.min(w, h) / rFactor
+    val lSize : Float = Math.min(w, h) / lineSizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val gap : Float = deg / parts
+    save()
+    translate(w / 2, h + r - (h / 2 + r) * sf1)
+    rotate(rot * sf3)
+    drawCircle(0f, 0f, r, paint)
+    for (j in 0..(lines - 1)) {
+        save()
+        rotate(gap * j)
+        drawLine(r, 0f, r + lSize * sf2, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawRBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawRisingBallLiner(scale, w, h, paint)
+}
