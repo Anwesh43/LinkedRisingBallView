eb6b20d360af24bfcaf9ce8150f811192c684588
Anwesh43 <anweshthecool0@gmail.com>
2020-11-20 09:10:44 +0530

    adding constants and libraries

--- RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
+++ RisingBallLinerView/src/main/java/com/example/risingballlinerview/RisingBallLinerView.kt
@@ -1,2 +1,25 @@
 package com.example.risingballlinerview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#3F51B5",
+    "#4CAF50",
+    "#FF9800",
+    "#009688"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
